711bfa8a53a0f409a4ab477deddcff8971b45005
kilkuwilku <56803463+kilkuwilku@users.noreply.github.com>
2020-01-31 19:26:01 +0100

    new style

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const game = new Game();
 
 document.getElementById('begin-game').addEventListener('click', function() {
     game.startGame();
-    
     this.style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
+    document.getElementById('logo').style.display = "none";
+    
     });
