7e08fcc6d64e4bdb238d9f5e388a91e6d46d2614
kilkuwilku <56803463+kilkuwilku@users.noreply.github.com>
2020-02-04 10:43:54 +0100

    Update Token.js

--- js/Token.js
+++ js/Token.js
@@ -1,5 +1,5 @@
 class Token {
-    constructor(owner, index){
+    constructor(index, owner){
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
